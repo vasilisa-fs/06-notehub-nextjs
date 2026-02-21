6eba1fde715f89472c3b80e513d69d0945402676
Vasilisa-fs <vasilisa.bychkova.fs@gmail.com>
2026-02-21 22:47:31 +0200

    Done

---
+++ app/notes/[id]/page.tsx
@@ -0,0 +1,29 @@
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
+import { fetchNoteById } from '@/lib/api';
+import NoteDetailsClient from './NoteDetailsClient';
+
+interface NoteDetailsProps {
+  params: Promise<{ id: string }>;
+}
+
+const NoteDetails = async ({ params }: NoteDetailsProps) => {
+  const queryClient = new QueryClient();
+  const { id } = await params;
+
+  await queryClient.prefetchQuery({
+    queryKey: ['note', id],
+    queryFn: () => fetchNoteById(id),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NoteDetailsClient />
+    </HydrationBoundary>
+  );
+};
+
+export default NoteDetails;
