9656c576cdb020f11b1477d68b2adc1fb9307daa
Vasilisa-fs <vasilisa.bychkova.fs@gmail.com>
2026-03-02 23:34:19 +0200

    Rename

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -4,15 +4,15 @@ import {
   QueryClient,
 } from '@tanstack/react-query';
 import { fetchNoteById } from '@/lib/api';
-import NoteDetailsClient from './NoteDetailsClient';
+import NoteDetailsClient from './NoteDetails.client';
 
 interface NoteDetailsProps {
   params: Promise<{ id: string }>;
 }
 
 const NoteDetails = async ({ params }: NoteDetailsProps) => {
-  const queryClient = new QueryClient();
   const { id } = await params;
+  const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
     queryKey: ['note', id],
